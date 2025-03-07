5802f879374fcaa43946a01d15b275f2e52f212d
AE-Team-iq <84102237+AE-Team-iq@users.noreply.github.com>
2025-03-07 16:45:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,8 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 # إعدادات YouTube Data API
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+if not YOUTUBE_API_KEY:
+    raise ValueError("YouTube API key is missing. Please set YOUTUBE_API_KEY in .env file.")
 
 # إعدادات التسجيل
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
